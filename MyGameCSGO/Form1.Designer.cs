d858f8582a675e872f6f35db065d077e2b11507c
Андрей <see320m@gmail.com>
2022-03-13 22:22:44 +0300

    Добавьте файлы проекта.

---
+++ MyGameCSGO/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MyGameCSGO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(367, 38);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(26, 29);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "0";
+            this.scoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(308, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кол-во очков";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyGameCSGO.Resource1.MyBackGround;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MyGameCSGO/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGameCSGO
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap HandlerTexture = Resource1.Handler,
+                      TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point(200, 200);
+        private Point _direction = Point.Empty;
+        //текущее количество очков, которое изначально равно 0
+        private int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //добавляю атрибуты для того чтобы избежать мерцания картинок 
+            SetStyle(ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.AllPaintingInWmPaint
+                | ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Refresh();//для перерисовки формы, для отображения движения
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //каждый тик таймера меняет директорию на какое то случайне число
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics a = e.Graphics;
+            //Для определения курсора на форме 
+            var localPosition = this.PointToClient(Cursor.Position);
+            //уеличиваю скорость передвижениния круга по осям 
+            _targetPosition.X += _direction.X * 10;
+            _targetPosition.Y += _direction.Y * 10;
+            if (_targetPosition.X < 0 || _targetPosition.X > 500)
+            {
+                //если условие круг выходит за рамки формы, то он отскакивает(меняет направление)
+                _direction.X *= -1;
+
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                //если условие круг выходит за рамки формы, то он отскакивает
+                _direction.Y *= -1;
+            }
+
+            //проежуточная точка 
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y)); //сумма квадратов катетов, для работы системы начисления очков 
+
+            //если попадает в круг, то идет начиление очков 
+            if (distance < 10)//точность попадания в круг
+            {
+                AddScore(10);
+            }
+            
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            //для показа кругов на форме    
+            a.DrawImage(HandlerTexture, handlerRect);
+            a.DrawImage(TargetTexture, targetRect);
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = _score.ToString();
+
+        }
+    }
+}
